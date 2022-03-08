7f94d5048722dc1988e14e797e18c8b7ed0afb4f
samomri <samquantadev@gmail.com>
2022-03-08 15:45:07 -0500

    axios and quote display

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [quote,setQuote] = useState('');
+  const [author,setAuthor] = useState('');
+  const url = 'https://type.fit/api/quotes';
+
+  useEffect(() => {
+    fetchQuotes();
+  },[]);
+
+  async function fetchQuotes() {
+    const response = await axios.get(url);
+    const quote = response.data;
+    const id = Math.floor(Math.random()*1642)
+    console.log(quote[id])
+
+    setQuote(quote[id].text);
+    setAuthor(quote[id].author);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="app">
+        <div className="card">
+          <h3 className="heading">{quote}</h3>
+          <p className="author">-- {author}</p>
+          <button className="button" onClick={fetchQuotes}>
+            <span>GIVE ME A QUOTE!</span>
+          </button>
+        </div>
+      </div>
   );
 }
 

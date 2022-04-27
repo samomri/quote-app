f7a1db94c893a7762d8a37042eea16f151edcbd4
samomri <samquantadev@gmail.com>
2022-04-27 00:54:42 -0400

    Add Timer

--- src/App.js
+++ src/App.js
@@ -6,33 +6,36 @@ function App() {
 
   const [quote,setQuote] = useState('');
   const [author,setAuthor] = useState('');
+  const [time, setTime] = useState(Date.now());
 
   const url = 'https://type.fit/api/quotes';
 
   useEffect(() => {
+    const interval = setInterval(() => setTime(Date.now()), 15000);
     fetchQuotes();
-  },[]);
+    return () => {
+      clearInterval(interval);
+    };
+  },[time]);
 
   async function fetchQuotes() {
     const response = await axios.get(url);
     const quote = response.data;
     const id = Math.floor(Math.random()*1642);
-    console.log(quote)
-    console.log(quote[id]);
 
     setQuote(quote[id].text);
     quote[id].author ? setAuthor(quote[id].author) : setAuthor('Unknown');
   }
   return (
     <div className="app">
-        <div className="card">
-          <h3 className="heading">{quote}</h3>
-          <p className="author">- {author} -</p>
-          <button className="button" onClick={fetchQuotes}>
-            <span>GIVE ME A QUOTE!</span>
-          </button>
-        </div>
+      <div className="card">
+        <h3 className="heading">{quote}</h3>
+        <p className="author">- {author} -</p>
+        <button className="button" onClick={fetchQuotes}>
+          <span>GIVE ME A QUOTE!</span>
+        </button>
       </div>
+    </div>
   );
 }
 
